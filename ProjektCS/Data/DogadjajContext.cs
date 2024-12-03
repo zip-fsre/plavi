866a7849e906fbb9d80aee418766b925223f5149
MataMajsta <maricmate30@gmail.com>
2024-12-03 16:20:38 +0100

    Inicijalizacija projekta + jedan kontroler završen

---
+++ ProjektCS/Data/DogadjajContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProjektCS.Models;
+
+namespace ProjektCS.Data
+{
+    public class DogadjajContext : IdentityDbContext
+    {
+        public DogadjajContext(DbContextOptions<DogadjajContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ProjektCS.Models.DOGADJAJ> DOGADJAJ { get; set; } = default!;
+    }
+}
---
+++ ProjektCS/Models/DOGADJAJ.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using ProjektCS.Data;
+
+namespace ProjektCS.Models
+{
+    public class DOGADJAJ
+    {
+        public int ID { get; set; }
+        public string? NAZIV { get; set; }
+        public DateTime? DATUM { get; set; }
+        public string? LOKACIJA { get; set; }
+        public string? SVRHA { get; set; }
+        public string? IME_ORGANIZATORA { get; set; }
+        public string? KONTAKT_ORGANIZATORA { get; set; }
+        public int? BROJ_GOSTIJU { get; set; }
+    }
+
+    
+}
